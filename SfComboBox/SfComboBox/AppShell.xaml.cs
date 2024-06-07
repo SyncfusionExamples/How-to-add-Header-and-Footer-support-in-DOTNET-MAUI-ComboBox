c662fd0fa500639cd403286131f13c3ccc4bfce7
Swetha <swetha.sf3977@gmail.com>
2024-06-07 16:57:30 +0530

    Updated ComboBox sample

---
+++ SfComboBox/SfComboBox/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SfComboBox;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SfComboBox/SfComboBox/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using Syncfusion.Maui.Inputs;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SfComboBox;
+
+public partial class MainPage : ContentPage {
+    SocialMediaViewModel socialMediaViewModel = new SocialMediaViewModel();
+    public MainPage() {
+
+        InitializeComponent();             
+    }  
+}
+
+public class SocialMedia {
+    public string Name { get; set; }
+    public int ID { get; set; }
+}
+
+//ViewModel.cs
+public class SocialMediaViewModel {
+ 
+    public ObservableCollection<SocialMedia> SocialMedias { get; set; }
+    public SocialMediaViewModel() {
+        this.SocialMedias = new ObservableCollection<SocialMedia>();
+        this.SocialMedias.Add(new SocialMedia() { Name = "Facebook", ID = 0 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "Google Plus", ID = 1 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "Instagram", ID = 2 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "LinkedIn", ID = 3 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "Skype", ID = 4 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "Telegram", ID = 5 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "Televzr", ID = 6 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "Tik Tok", ID = 7 });
+        this.SocialMedias.Add(new SocialMedia() { Name = "Tout", ID = 8 });
+    }
+
+}
+
